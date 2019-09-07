0bf892ec054f3e4911923561f765216e490a3d50
LukinEgor <lukin.net@gmail.com>
2019-09-07 13:39:55 +0300

    Add env vars for connection to mysql

--- lw/core/settings/base.py
+++ lw/core/settings/base.py
@@ -91,11 +91,13 @@ WSGI_APPLICATION = 'lw.core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': 'db',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators

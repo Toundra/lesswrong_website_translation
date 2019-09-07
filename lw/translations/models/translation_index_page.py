97b6ab480ac3a005ee94826e2a0808317231aa9d
Vyacheslav Matyukhin <me@berekuk.ru>
2019-09-07 16:47:00 +0300

    translations: rename fields, minimal jinja

--- lw/translations/models/translation_index_page.py
+++ lw/translations/models/translation_index_page.py
@@ -4,9 +4,18 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
+from .book_page import BookPage
+
 class TranslationIndexPage(Page):
     intro = RichTextField(blank=True)
 
+    def get_context(self, request):
+        context = super().get_context(request)
+
+        context['books'] = BookPage.objects.child_of(self).live()
+        return context
+
+    template = 'translations/index.html'
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
     ]

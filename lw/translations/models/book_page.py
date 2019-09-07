be72159f60fcb528f4458afc4a079a8e249957b8
Vyacheslav Matyukhin <me@berekuk.ru>
2019-09-07 16:47:00 +0300

    translations: rename fields, minimal jinja

--- lw/translations/models/book_page.py
+++ lw/translations/models/book_page.py
@@ -4,28 +4,25 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
-class TranslationPage(Page):
+class BookPage(Page):
     body = RichTextField(blank=True)
 
-    field_author = models.CharField(max_length=100)
-    field_translators = models.CharField(max_length=100)
-    field_original_link = models.CharField(max_length=100)
-    field_rfatz_id = models.PositiveSmallIntegerField()
-    field_on_vk = models.BooleanField()
-    field_readthesequences_link = models.CharField(max_length=100)
-    field_rating = models.PositiveSmallIntegerField()
+    author = models.CharField(max_length=100, blank=True)
+    translators = models.CharField(max_length=100, blank=True)
+    original_link = models.CharField(max_length=100, blank=True)
+    rfatz_id = models.PositiveSmallIntegerField(null=True, blank=True)
+    on_vk = models.BooleanField(default=False)
+    readthesequences_link = models.CharField(max_length=100, blank=True)
 
-    # field_illustrations
-    # field_ref_audio
+    # illustrations
+    # ref_audio
 
     content_panels = Page.content_panels + [
-        FieldPanel('title'),
         FieldPanel('body', classname="full"),
-        FieldPanel('field_author'),
-        FieldPanel('field_translators'),
-        FieldPanel('field_original_link'),
-        FieldPanel('field_rfatz_id'),
-        FieldPanel('field_on_vk'),
-        FieldPanel('field_readthesequences_link'),
-        FieldPanel('field_rating'),
+        FieldPanel('author'),
+        FieldPanel('translators'),
+        FieldPanel('original_link'),
+        FieldPanel('rfatz_id'),
+        FieldPanel('on_vk'),
+        FieldPanel('readthesequences_link'),
     ]

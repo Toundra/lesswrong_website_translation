5f8f96b2841c5c0ba81566f8775a84aa91ee0177
Vyacheslav Matyukhin <me@berekuk.ru>
2019-10-06 15:20:59 +0300

    home model fields - body, site_areas

--- lw/home/models.py
+++ lw/home/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 
 from wagtail.core.models import Page
-
+from wagtail.core import fields
+from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 
 class HomePage(Page):
-    pass
+    body = fields.RichTextField(blank=True)
+    site_areas = fields.StreamField([
+        (
+            'site_area',
+            blocks.StructBlock([
+                ('header', blocks.CharBlock(classname='title')),
+                ('text', blocks.CharBlock()),
+                ('image', ImageChooserBlock()),
+            ])
+        )
+    ], blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+        StreamFieldPanel('site_areas'),
+    ]

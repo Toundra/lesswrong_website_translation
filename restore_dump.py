71f7322f513441a5d1c8f2b00b7118c4c4ab9772
Lukin Egor <lukin.net@gmail.com>
2020-12-12 15:48:22 +0400

    added script for restore dump

---
+++ restore_dump.py
@@ -0,0 +1,26 @@
+import boto3
+import os
+
+AWS_REGION_NAME=os.environ.get('AWS_REGION_NAME', None)
+AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID', None)
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY', None)
+
+DB_NAME=os.environ.get('DB_NAME', None)
+DB_HOST=os.environ.get('DB_HOST', None)
+DB_USER=os.environ.get('DB_USER', None)
+DB_PASSWORD=os.environ.get('DB_PASSWORD', None)
+
+BUCKET_NAME = os.environ.get('BUCKET_NAME', None)
+DUMP_NAME = os.environ.get('DUMP_NAME', None)
+LOCAL_DUMP_NAME = "lw-dump.sql"
+
+s3 = boto3.client('s3',
+                  region_name=AWS_REGION_NAME,
+                  aws_access_key_id=AWS_ACCESS_KEY_ID,
+                  aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+                  )
+
+s3.download_file(BUCKET_NAME, OBJECT_NAME, LOCAL_DUMP_NAME)
+
+restore_dump_cmd = f'mysql {DB_NAME} -h {DB_HOST} -u {DB_USER} -p {DB_PASSWORD} < {LOCAL_DUMP_NAME}'
+os.system(restore_dump_cmd)
